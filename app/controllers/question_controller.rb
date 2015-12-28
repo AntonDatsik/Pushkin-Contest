2534dd235ed89460c299768abf5ed9fb5794ab22
AntonDatsik <Anton.Dacik@gmail.com>
2015-12-28 17:33:56 +0300

    implemented app

---
+++ app/controllers/question_controller.rb
@@ -0,0 +1,184 @@
+class QuestionController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def registration
+    q = params[:question]
+    # @token = Token.new(token: params[:token])
+    # @token.save
+    file = File.read("db/poems-full.json")
+    hash = JSON.parse(file)
+    @answer = 'снежные'
+    render json: {answer: @answer}
+  end
+
+
+  def quiz
+    @question = params[:question]
+
+    @id = params[:id]
+    @level = params[:level]
+    file = File.read("db/poems-full.json")
+    @hash = JSON.parse(file)
+    @answer = ""
+    case @level
+    when '1'
+      @answer = level1
+    when '2'
+      @answer = level2(@question)
+    when '3'
+      @answer = level3(@question)
+    end
+    
+    @token = Token.last
+    uri = URI("http://pushkin.rubyroid.by/quiz")
+
+    parameters = {
+      answer: @answer,
+      token: @token,
+      task_id:  @id
+    }
+
+    Net::HTTP.post_form(uri, parameters) 
+  end
+
+  private
+  def level1
+    answer = ''
+    @hash.each do |k|
+       k[1].map do |str|
+        if str.include?(@question) then
+          answer = k[0]
+          break
+        end
+      end
+    end
+    answer
+  end
+
+  def level2(question)
+    answer = ''
+    @question.downcase!
+    q = question.split('word')
+
+    @hash.each do |k|
+      k[1].map do |str|
+        str.downcase!
+        if q.count >= 2 then
+          if str.include?(q[0]) || str.include?(q[1]) then
+            question_words = question.split(" ")
+            str_words = str.split(" ")
+              
+            for i in 0..str_words.count - 1 
+              if !str_words[i].eql?(question_words[i])
+                answer = str_words[i].delete(",").delete(".").delete("?")
+                return answer
+              end
+            end
+          else
+            if str.include?(q[0]) then
+              question_words = question.split(" ")
+              str_words = str.split(" ")
+
+              for i in 0..str_words.count - 1 
+                if !str_words[i].eql?(question_words[i])
+                  answer = str_words[i].delete(",").delete(".").delete("?")
+                  return answer
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+
+    answer
+  end
+
+  def level3(question)
+    answer = ''
+    temp = question.split('\n')
+    q1 = temp[0]
+    q2 = temp[1]
+    q1.downcase!
+    q2.downcase!
+    q = q1.split('word')
+
+
+    @hash.each do |k|
+      for index in 0..k[1].count - 1  
+        str = k[1][index]
+        str.downcase!
+        if q.count >= 2 then
+          if str.include?(q[0]) || str.include?(q[1]) then
+            question_words = q1.split(" ")
+            str_words = str.split(" ")
+              
+            for i in 0..str_words.count - 1 
+              if !str_words[i].eql?(question_words[i])
+                answer = str_words[i].delete(",").delete(".").delete("?")
+                answer += ','
+                answer += next_line_with_word(k[1][index+1], q2)
+                return answer
+              end
+            end
+          else
+            if str.include?(q[0]) then
+              question_words = q1.split(" ")
+              str_words = str.split(" ")
+
+              for i in 0..str_words.count - 1 
+                if !str_words[i].eql?(question_words[i])
+                  answer = str_words[i].delete(",").delete(".").delete("?")
+                  answer += ','
+                  answer += next_line_with_word(k[1][index+1], q2)
+                  return answer
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+    answer
+  end
+
+  def next_line_with_word(str, q)
+    answer = ''
+    str.downcase!
+    question = q
+    q = q.split("word")
+
+    if q.count >= 2 then
+      if str.include?(q[0]) || str.include?(q[1]) then
+        question_words = question.split(" ")
+        str_words = str.split(" ")
+                
+        for i in 0..str_words.count - 1 
+          if !str_words[i].eql?(question_words[i])
+            answer = str_words[i].delete(",").delete(".").delete("?")
+            return answer
+          end
+        end
+      end
+    else
+      if str.include?(q[0]) then
+        question_words = question.split(" ")
+        str_words = str.split(" ")
+
+        for i in 0..str_words.count - 1 
+          if !str_words[i].eql?(question_words[i])
+            answer = str_words[i].delete(",").delete(".").delete("?")
+            return answer
+          end
+        end
+      end
+    end
+    answer
+  end
+
+  def level4
+  end
+
+  def level5
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PushkinContest::Application.config.secret_key_base = 'a79147a90e6cf393e1377775cec9275e8ca21969f87369b350100fea17b41b51f07ce063a17b5d9511a655db04605acd6f1cb9dcf6c47f59e865c93845de7da2'
