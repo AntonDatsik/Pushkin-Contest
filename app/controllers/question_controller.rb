7cafe1599408246a647efefc48f7c545f1e45a21
AntonDatsik <Anton.Dacik@gmail.com>
2016-01-02 11:44:14 +0300

    fixed token bug

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -39,7 +39,7 @@ class QuestionController < ApplicationController
       @answer = level8(@question)
     end
     
-    @token = Token.last.token
+    @token = Token.first.token
     uri = URI("http://pushkin.rubyroid.by/quiz")
 
     parameters = {
